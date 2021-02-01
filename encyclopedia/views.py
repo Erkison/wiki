36072fee83a182f236e599b37f868b8e24a5d4c8
Erkison Odih <erkisonodih@gmail.com>
2021-02-01 21:39:05 +0100

    add response page for entry not found

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -9,7 +10,11 @@ def index(request):
     })
 
 def entry_page(request, entry_title):
-    return render(request, "encyclopedia/entry_page.html", {
-        "entry": util.get_entry(entry_title),
-        "entry_title": entry_title
-    })
+    entry = util.get_entry(entry_title)
+    if entry == None:
+        return HttpResponse("The requested entry does not exist.")
+    else: 
+        return render(request, "encyclopedia/entry_page.html", {
+            "entry": util.get_entry(entry_title),
+            "entry_title": entry_title
+        })
