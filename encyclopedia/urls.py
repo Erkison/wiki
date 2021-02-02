c8d2497e8cf4ccad27aa6bf05c656e73fc2ddf85
Erkison Odih <erkisonodih@gmail.com>
2021-02-02 20:12:01 +0100

    update index page with dynamic entry links

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry_title>", views.entry_page, name="entry_page")
+    path("<str:entry_title>", views.entry_page, name="entry_title")
 ]
